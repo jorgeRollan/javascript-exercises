55953813585b8670da49870bb72ef49973d41196
Jorge Rollan <jorgerollandiez@gmail.com>
2022-02-07 17:34:10 +0100

    make exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, value) {
+    let repeatedString = "";
+    if (value >= 0) {
+        for (let a = 0; a < value; a++) {
+            repeatedString = repeatedString.concat(string);
+        }
+        return repeatedString;
+    }
+    else return 'ERROR'
 };
 
 // Do not edit below this line
