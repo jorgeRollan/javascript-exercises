57ac0b4b6a4e842456628e316b8167992c4ee8a6
Jorge Rollan <jorgerollandiez@gmail.com>
2022-02-08 00:52:26 +0100

    make exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let array = args.slice(0,1).flat();
+    let values=args.slice(1);
+    values.forEach(element => {
+    array=array.filter(value=>(typeof value != typeof element)&&(value==element)||(value!=element))
+    });
+    return array;
+    
+}
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
