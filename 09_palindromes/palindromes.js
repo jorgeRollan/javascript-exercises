5a3211787e650894703979fc7538c6e2bc963443
Jorge Rollan <jorgerollandiez@gmail.com>
2022-02-08 19:47:05 +0100

    make exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    palindrome = (palindrome.replace(/[^a-zA-Z ]/g, "")).split(" ").join("").split("");
+    let palindromeReverse = palindrome.reverse().slice("");
+    palindrome.reverse();
+    let a = 0;
+    let equal = true;
+    while (a < palindrome.length && equal == true) {
+        if (palindrome[a].toLowerCase() != palindromeReverse[a].toLowerCase()) {
+            equal = false;
+        }
+        a=a + 1;
+    }
+    return equal;
 };
 
 // Do not edit below this line
