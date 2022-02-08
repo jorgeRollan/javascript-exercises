45382d83c53b98b95d54ef25372a88ea428e1b85
Jorge Rollan <jorgerollandiez@gmail.com>
2022-02-08 15:34:09 +0100

    make exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
-
-};
+const leapYears = function (...args) {
+    if (args.length == 1 && typeof args[0] == 'number') {
+        let fecha = args[0];
+            if (fecha % 100 == 0) {
+                if (fecha % 400 == 0) {
+                    return true;
+                }
+                else return false;
+            }
+            else {
+                if (fecha % 4 == 0) {
+                    return true;
+                }
+                else return false;
+            }
+        }
+    else return "ERROR";
+}
 
 // Do not edit below this line
 module.exports = leapYears;
