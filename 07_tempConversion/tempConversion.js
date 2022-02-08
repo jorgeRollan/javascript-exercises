982ffc0ca228bc7aac17787d93c02afef0f048c5
Jorge Rollan <jorgerollandiez@gmail.com>
2022-02-08 16:26:56 +0100

    make exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = function (number) {
 
+  let resultado = ((number - 32) * 5 / 9).toFixed(1);
+  return Number(resultado);
 };
 
-const ctof = function() {
-
+const ctof = function (number) {
+  let resultado = ((number * 9 / 5) + 32).toFixed(1);
+  return Number(resultado);
 };
 
 // Do not edit below this line
