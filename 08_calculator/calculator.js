3d25b268e0538b3c56ab898aac6ccea5eab7dbff
Jorge Rollan <jorgerollandiez@gmail.com>
2022-02-08 16:56:32 +0100

    make exercise 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function (number1, number2) {
+  return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function (number1, number2) {
+  return number1 - number2
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  let sum = 0;
+  numbers.forEach(element => {
+    sum = sum + element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  let mul=1
+  numbers.forEach(element => {
+    mul = mul * element;
+  });
+  return mul;
 };
 
-const power = function() {
-	
+const power = function (number1, number2) {
+  if (number2 == 0) {
+    return 1;
+  }
+  else {
+    if (number2 > 0) {
+      return number1 * power(number1, number2 - 1);
+    }
+    else { return number1 * power(number1, number2 + 1); }
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number == 0) {
+    return 1;
+  }
+  else if (number == 1) {
+    return 1;
+  }
+  else if(number==2){
+    return 2;
+  }
+  else {
+    return number * factorial(number-1);
+  }
 };
 
 // Do not edit below this line
