1db3b8b8e062b0c5f6bd5da3b9726a240bc36a34
Jorge Rollan <jorgerollandiez@gmail.com>
2022-02-08 20:02:11 +0100

    make exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    let fib = Number(number);
+    if (number < 0) {
+        return "OOPS";
+    }
+    else {
+        if (fib == 0) {
+            return 0;
+        }
+        else if (fib == 1) {
+            return 1;
+        }
+        else { return (fibonacci(fib - 1) + fibonacci(fib - 2)) }
+    }
 };
 
 // Do not edit below this line
