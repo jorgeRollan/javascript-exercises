9af09b432257ca77d6b6c11eba571153f8521b39
Jorge Rollan <jorgerollandiez@gmail.com>
2022-02-08 15:03:43 +0100

    make exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function (number1, number2) {
+    let n1 = number1;
+    let n2 = number2;
+
+    if (number1 > 0 && number2 > 0 && typeof number2 == 'number') {
+        if (number1 > number2) {
+            n1 = number2;
+            n2 = number1;
+        }
+        if (n2 > n1) {
+            return n2 + sumAll(n1, n2 - 1);
+        }
+        else { return n1; }
+    }
+    else return "ERROR";
 
 };
 
